a24c43218e2e124601eeea69a4462a9bfad4d09c
ShawnZain <shawnzainpatel@gmail.com>
2022-11-13 00:49:12 +0300

    My first python project; a simple mad libs game

---
+++ mad-libs-game.py
@@ -0,0 +1,51 @@
+
+print("\nHey there. Weclome to our fun Mad Libs Game.")
+
+adjective1 = input("\nEnter an adjective: ")
+noun1 = input ("Enter a noun: ")
+plural_noun1 = input ("Enter a plural noun: ")
+name = input ("Enter a name of a person in the room (Female): ")
+adjective2 = input ("Enter another adjective: ")
+clothes = input ("Enter an article of clothing: ")
+noun2 = input ("Enter another noun: ")
+city = input ("Name a city: ")
+plural_noun2 = input ("Enter another plural noun: ")
+adjective3 = input ("Enter another adjective: ")
+body_part1 = input ("Enter a body part: ")
+alphabet = input ("Enter a letter of the alphabet: ")
+celebrity = input ("Enter a celebrity's name: ")
+plural_noun3 = input ("Enter another plural noun: ")
+adjective4 = input ("Enter another adjective: ")
+place = input ("Enter a the name of a place: ")
+body_part2 = input ("Enter another body part: ")
+adjective5 = input ("Enter another adjective: ")
+verb = input ("Enter a verb: ")
+plural_noun4 = input ("Enter another plural noun: ")
+number = input ("Enter a number: ")
+
+
+print(
+    "\nThere are many " + adjective1 +
+    " ways to chose a/an " + noun1 + " to read."
+    "\nFirst you could ask for recommendations from your friends and "
+    + plural_noun1 + ". Just don't ask Aunt "
+    + name + " - she only reads " +adjective2 +
+    " books with " + clothes + "-ripping goddesses on the cover."
+    "\nIf your friends and family are no help, try checking out the "
+    + noun2 + " Review in The " + city + " Times."
+    " If the " + plural_noun2 + " featured there are too "
+    + adjective3 + " for your taste, try something a litte more low-"
+    + body_part1 + ", like " + alphabet + ":"
+    "\nThe " + celebrity + " Magazine, or " + plural_noun3 +
+    " Magazine. You could also choose a book the " +
+    adjective4 + "-fashioned way. Head to your local library or "
+    + place + " and browse the shelves until something catches "
+    "your " + body_part2 + ". Or you could save yourself a whole lot of "
+    + adjective4 + " trouble and log on to www.bookish.com, "
+    "the " +adjective5 + " new website to " + verb +
+    " for books!\nWith all the time you'll save not having to search for "
+    + plural_noun4 + ", you can read " + number + " more books!"
+
+)
+
+print("\nThank you for playing the game.")
